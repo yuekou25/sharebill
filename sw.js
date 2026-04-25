bcdcce21484a247e1afcce9ed0427ae5a5c91235
yuekou25 <nph5252@gmail.com>
2026-04-26 00:13:05 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,14 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('sharebill-store').then((cache) => cache.addAll([
+      './',
+      './index.html',
+    ])),
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request)),
+  );
+});
